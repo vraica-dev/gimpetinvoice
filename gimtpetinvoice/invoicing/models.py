8ff62361771c28f693375d8278d5c096d2c155cb
vraica-dev <rvg.vali@gmail.com>
2024-11-02 16:50:39 +0000

    created city model and mig for populating the  model

--- gimtpetinvoice/invoicing/models.py
+++ gimtpetinvoice/invoicing/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models import fields
 
 # Create your models here.
+class City(models.Model):
+    name = fields.CharField(max_length=90, blank=False, null=False)
+    county = fields.CharField(max_length=90, blank=False, null=False)
+    county_code = fields.CharField(max_length=90, blank=False, null=False)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'cities'
+
+    def __str__(self):
+        return str(self.name)
